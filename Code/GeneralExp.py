3b64ac1379b2b55b3a69d501beed6a2c1a0ac028
rbostandoust <r.bostandoust@gmail.com>
2025-05-09 18:17:28 +0000

    box plots added!

--- Code/GeneralExp.py
+++ Code/GeneralExp.py
@@ -498,8 +498,8 @@ solver_max_timeout_in_seconds = 1 * 60
 """
 Carbon Intensity
 """
-location = "California"
-# location = "AU-SA"
+# location = "California"
+location = "AU-SA"
 
 # loading the whole th trace
 carbon_trace = {}
@@ -521,7 +521,7 @@ Sampling from the job pool and determining arrival epochs
 """
 num_instances = 2000
 num_jobs = 10 # per instance
-num_machines = 10 # per instance
+num_machines = 5 # per instance
 num_operations_per_job = 3
 mean_duration_per_op_in_epoch = 7
 experiment_type = "Homogen"
@@ -723,9 +723,9 @@ def main(experiment_type, start_date = pd.to_datetime("2024-01-01").date(), num_
     
 # main(experiment_type = experiment_type)
 ###########
-run_ver = 7
-# candidate_makespan_slack_coeff = [1, 1.5, 2]
-candidate_makespan_slack_coeff = [1]
+run_ver = 0
+candidate_makespan_slack_coeff = [1, 1.5, 2]
+# candidate_makespan_slack_coeff = [1]
 #-----
 start_date = pd.to_datetime("2024-01-01").date()
 total_days = 360
