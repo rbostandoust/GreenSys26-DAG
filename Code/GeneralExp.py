11beebe28cda65f720335b290d1badcda482425a
rbostandoust <r.bostandoust@gmail.com>
2025-05-10 21:04:57 +0000

    completion time coded added!

--- Code/GeneralExp.py
+++ Code/GeneralExp.py
@@ -740,7 +740,7 @@ def main(experiment_type, start_date = pd.to_datetime("2024-01-01").date(), num_
     
 # main(experiment_type = experiment_type)
 ###########
-run_ver = 0
+run_ver = 7
 candidate_makespan_slack_coeff = [1, 1.5, 2]
 # candidate_makespan_slack_coeff = [1]
 #-----
